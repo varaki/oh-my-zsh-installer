5b296bd12a7c5f0e5ee7e1e3f13eed015cd81b45
Ákos Várady <6222249-varaki@users.noreply.gitlab.com>
2020-06-16 08:50:55 +0000

    Update install_oh_my_zsh.sh

--- install_oh_my_zsh.sh
+++ install_oh_my_zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -) --unattended"
+sh -c "$(wget https://gitlab.com/varaki/oh-my-zsh-installer/-/raw/master/install_oh_my_zsh.sh -O -) --unattended"
 
 # Clone plugins
 mkdir -p $HOME/.oh-my-zsh/custom/{plugins,themes}
@@ -9,4 +9,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Install custom theme
-wget "https://github.com/varaki/bubblified-varaki.zsh-theme/raw/master/bubblified-varaki.zsh-theme" -O - > $HOME/.oh-my-zsh/custom/themes/bubblified-varaki.zsh-theme
+wget "https://gitlab.com/varaki/bubblified-varaki.zsh-theme/-/raw/master/bubblified-varaki.zsh-theme" -O - > $HOME/.oh-my-zsh/custom/themes/bubblified-varaki.zsh-theme
